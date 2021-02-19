5884d9c0c3b4ed66f76418a9640aaa20fc2d6e28
DHEVISHREE <cloudsupport1@amvionlabs.in>
2021-02-19 12:30:39 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Dhevi Shree will be a Devops Expert'));
+app.get('/', (req, res) => res.send('Dhevi Shree'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
