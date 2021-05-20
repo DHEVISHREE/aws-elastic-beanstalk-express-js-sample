96f7ef071b9ed9772067015d1d3bb2f598f3d8c3
DHEVISHREE <cloudsupport1@amvionlabs.in>
2021-05-20 14:51:09 +0530

    change message

--- app.js
+++ app.js
@@ -3,138 +3,62 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Test 1
-<html>
-    <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Stanson Rubber</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;900&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-        <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="css/responsive.css">
-        <style type="text/css">
-        .menu ul li a {
-    transition: all .3s ease;
-        transition-delay: 0s;
-    transition-delay: 0s;
-    padding-bottom: 25px;
-}
+A successful DevOps roadmap is a must-have. To better understand why this is the case, let’s consider the results of a recent survey of more than 3,000 global technology professionals conducted by Puppet.
 
-        .navbar .menu a {
-    color: white;
-    text-decoration: none;
-    font-size: 30px;
-}
+Key takeaways from the Puppet “2018 State of DevOps Report” included:
 
-            .navbar .menu a:hover { color: #6a8bff; }
+There are many paths to DevOps failure. The DevOps journey is not linear, and organizations often struggle to effectively scale DevOps across all departments.
+Cross-team collaboration drives DevOps success. DevOps requires information sharing and collaboration among all departments.
+Automating security policy configurations is mission-critical. Highly evolved organizations are 24 times more likely to automate security policy configurations compared to other organizations.
+Additionally, Puppet identified six foundational stages that can be used to create a successful DevOps roadmap:
 
-.navbar .nav-layer {
-    background-color: #000d1a;
-}
+Stage 0: Build the Foundation
 
+Implement technologies and processes to drive information sharing and collaboration among DevOps team members. Then, DevOps team members can identify the best processes and approaches based on an enterprise, its key stakeholders and other factors.
 
-.demo {
-    background: #000d1a !important;
-    color: #fff;
-    height: 67px;
-}
+DevOps team members also must maintain flexibility as they implement assorted technologies and processes. That way, they can update these technologies and processes as needed to keep pace with a fast-moving global marketplace.
 
-.navbar {
-    position: relative;
-    height: 60px;
-    overflow: hidden;
-}
-                    
-.sticky {
-    position: fixed;
-    top: 0;
-    width: 100%;
-    z-index: 100;
-    background-color: #000d1a!important;
-    box-shadow: 0px 0px 10px 0px #333;
-}
-        </style>
+Stage 1: Normalize the Technology Stack
 
-    </head>
-    <body>
-        <div class="demo" id="sticky-head">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-xl-10 col-lg-10 col-md-9 col-9 no-padding">
-                <img src="images/logo-with-white-bg4.png" class="img-fluid">
-            </div>
-            <div class="col-xl-2 col-lg-2 col-md-3 col-3">
-                <div class="navbar">
-                    <span class="text-bold" style="line-height: 55px;">MENU</span>
-                    <nav class="menu">
-                        <div class="nav-layer"></div>
-                        <div class="logo-nav-layer"><img src="images/logo-white2.png" height="50px" class="img-fluid"></div>
-                        <ul>
-                            <li><a href="#link1">Activities</a>
-							<ul class="sub-menu">
-							<li id=link1>Our customer focus
-							 </li>
-							</ul>
-							
-							</li>
-							<br>
-                            <li><a href="#link2">The Groups</a>
-							<li id=link>Our customer focus
-							 </li>
-							</li>
-                            <li><a href="#">Products</a></li>
-                            <li><a href="#">Expertise</a></li>
-                            <li><a href="#">Newsroom</a></li>
-                            <li><a href="#">Careers</a></li>
-                        </ul>
-                    </nav>
-                    <a class="nav-toggle">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-  </div>
-  <div class="video-container">
-  <video autoplay="autoplay" muted="muted" loop="loop" id="video-bg" class="video-responsive" style="position: relative; z-index: -1;">
-     <source src="video/stock-footage-automotive-engineer-working-on-electric-car-chassis-platform-using-tablet-computer-with-augmented.mp4" type="video/mp4">
-    </video>
-</div>
+Transition to agile development methods and adopt version control, i.e. the first step toward continuous integration (CI) and continuous delivery (CD).
 
-<div style="position: absolute;z-index: 1; top:50%; left: 50%; margin-left:-405px; margin-top: -120px; width: 810px; height:240px;">
-    <div class="">
-      <div class="">
-<!--         <h1 class="display-3">Video Header</h1>
-        <p class="lead mb-0">With HTML5 Video and Bootstrap 4</p> -->
-        
-      </div>
-    </div>
-  </div>
+At this point, a DevOps team eliminates redundant systems and refactors applications. This allows the team to normalize its technology stack – something that may lead to faster, more efficient operations than ever before.
 
+Stage 2: Standardize and Reduce Variance
 
-<div style="width: 100%; display: block;">
-    <div style="text-align: center; width: 1300px; margin:0 auto;"><img src="images/home6.jpg" width="100%" alt=""></div>
-</div>
-<div style="width: 100%; display: block; background-color: #0026a9;">
-    <div style="text-align: center; width: 1300px; margin:0 auto;"><img src="images/footer-blue.jpg" width="100%" alt=""></div>
-</div>
+Reduce variance, continue to standardize an enterprise’s technology stack and limit the number of enterprise operating systems and technologies.
 
+A DevOps team must prioritize consolidation and collaboration. If a DevOps team works with different departments to eliminate excess operating systems and technologies, it can set the stage for long-lasting DevOps success.
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-<script src="js/custom.js"></script>
-<script src="js/wow.js"></script>
-<script>
-new WOW().init();
-</script>
-    </body>
-</html>
-'));
+Furthermore, a DevOps team must apply consistent management and deployment patterns across an enterprise’s applications. This helps minimize the risk of errors. It also helps drive faster application and service deployment and service quality improvements.
+
+Stage 3: Expand DevOps Practices
+
+Address enterprise pain points. To do so, a DevOps team must build applications and services and test infrastructure changes to ensure predictability and reliability.
+
+Over time, DevOps practices may result in a cultural transformation across an enterprise. These practices ultimately help a DevOps team achieve the optimal results, leading to increased trust between this team and various departments.
+
+As a DevOps team builds trust, it may receive permission to perform assorted tasks without executive approval. This helps eliminate bureaucratic overhead, resulting in more efficient workflows that benefit the DevOps team, the enterprise and its key stakeholders.
+
+Stage 4: Automate Infrastructure Delivery
+
+Automate systems configuration and provisioning. This limits the risk of developer throughout outpacing an enterprise’s operations. It also helps a DevOps team deliver systems that match development and quality assurance teams’ production environments.
+
+An automated infrastructure promotes self-service across myriad business departments. It often leads to increased enterprise efficiency and employee satisfaction, too.
+
+Stage 5: Provide Self-Service Capabilities
+
+Make resources available via self-service and automate incident response.
+
+Automation is valuable if DevOps team members understand its potential benefits. By using automation technologies and tools that support an enterprise’s operations, a DevOps team can deliver unprecedented self-service capabilities.
+
+Puppet also recommends that an enterprise consider the following factors as it deploys a DevOps roadmap:
+
+Automation: Start small with automation. As a DevOps team automates enterprise services, it may find innovative ways to automate an enterprise’s operations. At the same time, certain enterprise services may prove to be too complex or expensive to automate. And if the costs of automating certain enterprise services outweigh the benefits, a DevOps team should explore alternative solutions to achieve its desired results.
+Culture: A DevOps team can foster a cultural transformation that permeates across an enterprise. It can empower different departments with DevOps best practices to drive operational efficiency. As such, a DevOps team can play a pivotal role in the development of a successful enterprise culture.
+Measurement: Highly evolved organizations have significantly higher levels of automated business metrics available on demand in contrast to other organizations, according to Puppet. They also have the lowest level of manually gathered system metrics. Yet some metrics must be collected manually and tracked regularly. These metrics include enterprise revenue, renewal rates, customer acquisition costs, overhead costs and variable cost percentages.
+Sharing: Highly evolved organizations transition from sharing largely within individual teams to sharing across all departments, Puppet notes. Meanwhile, a DevOps team can share automation and deployment patterns across an enterprise. These patterns then can help an enterprise standardize its everyday operations.
+A systematic approach to DevOps is ideal. This approach enables a DevOps team to quickly identify and address potential problems before they get out of hand. Best of all, it allows a DevOps team to drive meaningful improvements across all levels of an enterprise.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
