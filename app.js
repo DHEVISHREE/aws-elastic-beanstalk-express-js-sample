46637e927f34acbc74bee10e49071740ea5caf58
DHEVISHREE <cloudsupport1@amvionlabs.in>
2021-05-20 14:44:25 +0530

    change message

--- app.js
+++ app.js
@@ -3,13 +3,6 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => res.send('Test 1'));
-jQuery.each( [ "toggle", "show", "hide" ], function( i, name ) {
-    var cssFn = jQuery.fn[ name ];
-    jQuery.fn[ name ] = function( speed, easing, callback ) {
-      return speed == null || typeof speed === "boolean" ?
-        cssFn.apply( this, arguments ) :
-        this.animate( genFx( name, true ), speed, easing, callback );
-    };
-  } );
+
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
