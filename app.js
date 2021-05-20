cb2aa0a43dc5668dffbcd9426b408392125a7aff
DHEVISHREE <cloudsupport1@amvionlabs.in>
2021-05-20 16:10:05 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('DevOps is a set of practices that combines software development and IT operations. It aims to shorten the systems development life cycle and provide continuous delivery with high software quality. DevOps is complementary with Agile software development; several DevOps aspects came from the Agile methodology'));
+app.get('/', (req, res) => res.send('DevOps'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
