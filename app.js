5c8ec5ef75c5d3a4d1473cdd48beae9a57406808
DHEVISHREE <cloudsupport1@amvionlabs.in>
2021-05-20 14:39:58 +0530

    change message

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Test'));
-
+app.get('/', (req, res) => res.send('Test 1'));
+$box
+        .removeClass('on')
+        .animate({
+          'width': '30%',
+          'opacity': 0
+        }, 200, 'swing', function () {
+          $box.removeAttr('style');
+          callback && callback();
+        });
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
